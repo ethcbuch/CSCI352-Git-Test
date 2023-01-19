6b901163fa11879bf60871d470a54f4e662731bb
Ethan Buchanan <ethcbuch@ut.utm.edu>
2023-01-18 19:32:36 -0600

    Finished first part of random num gen

--- CW 1.1/Program.cs
+++ CW 1.1/Program.cs
@@ -15,19 +15,30 @@ namespace CW_1._1
         Random rnd = new Random();
 
         private int randomNumber;
-        private string userNumber;
+        private string userNumberToConvert;
+        private int userNumber;
+        private int attemptsCount;
 
         public int inputUserNumber()
         {
-            userNumber = Console.ReadLine();
-            int a = Convert.ToInt32(userNumber);
+            Console.WriteLine("Guess a Number: ");
+            userNumberToConvert = Console.ReadLine();
+            userNumber = Convert.ToInt32(userNumberToConvert);
 
-            return a;
+            while (getUserNumber() > 100 || getUserNumber() < 0)
+            {
+                Console.WriteLine("Please choose a number between 0 and 100!");
+                Console.WriteLine("Guess a Number: ");
+                userNumberToConvert = Console.ReadLine();
+                userNumber = Convert.ToInt32(userNumberToConvert);
+            }
+
+            return userNumber;
         }
 
         public void findRandomNumber()
         {
-                randomNumber = rnd.Next(0,100);
+            randomNumber = rnd.Next(0, 100);
         }
 
         public int getRandomNumber()
@@ -35,23 +46,47 @@ namespace CW_1._1
             return randomNumber;
         }
 
-        public string getUserNumber()
+        public int getUserNumber()
         {
             return userNumber;
+        }
+
+        public void attemptsCounter()
+        {
+            attemptsCount++;
+        }
+
+        public int getAttemptsCount()
+        {
+            return attemptsCount;
         }
 
         static void Main(string[] args)
         {
             Program p = new Program();
 
-            Console.WriteLine("Input a number from 1-100: ");
+            Console.WriteLine("I am thinking of a number between 0-100. Guess the correct number to escape this program!");
             p.inputUserNumber();
-
-            Console.WriteLine("User number is " + p.getUserNumber());
-            Console.ReadKey();
-
             p.findRandomNumber();
-            Console.WriteLine("The random number is: " + p.getRandomNumber());
+
+            while (p.getRandomNumber() != p.getUserNumber())
+            {
+                if (p.getUserNumber() < p.getRandomNumber())
+                {
+                    Console.WriteLine("Aim Higher!");
+                    p.inputUserNumber();
+                    p.attemptsCounter();
+                }
+                else
+                {
+                    Console.WriteLine("Aim Lower!");
+                    p.inputUserNumber();
+                    p.attemptsCounter();
+                }
+
+            }
+            Console.WriteLine("You Won!");
+            Console.WriteLine("It took you " + p.getAttemptsCount() + " attempt(s) to guess the right number!");
             Console.ReadKey();
         }
     }
